4b54a4caa503dbaeb47900b9a4b3f0cb22d65a4a
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-03-02 15:27:13 -0300

    back

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,35 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    dishRes: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    dishScore: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    healthyScore: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    stepByStep: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true
+    },
+    img: {
+      type: DataTypes.STRING,
+      allowNull:true
+    }
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,145 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const axios = require("axios");
+const { Recipe, Diet } = require("../db");
+const { API_KEY } = process.env;
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+const getRecipesFromApi = async () => {
+  const apiUrl = await axios.get(
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true`
+  );
+  //   console.log(await apiUrl.data.results.analyzedInstructions);
+  const info = await apiUrl.data.results.map((e) => {
+    return {
+      id: e.id,
+      name: e.title,
+      dishRes: e.summary,
+      dishScore: e.spoonacularScore,
+      healthyScore: e.healthScore,
+      stepByStep: e.analyzedInstructions.map((e) =>
+        e.steps.map((el) => el.step)
+      ),
+      img: e.image,
+      vegetarian: e.vegetarian,
+      vegan: e.vegan,
+      glutenFree: e.glutenFree,
+      diets: e.diets.map((e) => e),
+    };
+  });
+  return info;
+};
 
+const getRecipesFromDb = async () => {
+  return await Recipe.findAll({
+    include: {
+      model: Diet,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
+};
+
+const getAllRecipes = async () => {
+  const recipesFromApi = await getRecipesFromApi();
+  const recipesFromDb = await getRecipesFromDb();
+  const allRecipes = recipesFromApi.concat(recipesFromDb);
+  return allRecipes;
+};
+
+const getAllDiets = async () => {
+  const diets = [
+    "gluten free",
+    "ketogenic",
+    "vegetarian",
+    "lacto ovo vegetarian",
+    "vegan",
+    "pescatarian",
+    "paleolithic",
+    "primal",
+    "fodmap friendly",
+    "dairy free",
+    "whole 30",
+  ];
+  diets.forEach((e) => {
+    Diet.findOrCreate({
+      where: { name: e },
+    });
+  });
+  const dietTypes = await Diet.findAll();
+  console.log(dietTypes);
+  return dietTypes;
+};
+
+// const getRecipesByQuery
+// const getRecipesByParams
+// const getTypesOfRecipes
+// const PostRecipe
+router.get("/recipes", async (req, res) => {
+  const { name } = req.query;
+  let recipes = await getAllRecipes();
+  if (name) {
+    let queryRecipe = await recipes.filter((e) =>
+      e.name.toLowerCase().includes(name.toLowerCase())
+    );
+    if (queryRecipe.length) {
+      res.status(200).send(queryRecipe);
+    } else {
+      res.status(404).send("Esta receta no existe");
+    }
+  } else {
+    res.status(200).send(recipes);
+  }
+});
+
+router.get("/recipes/:id", async (req, res) => {
+  const { id } = req.params;
+  let recipes = await getAllRecipes();
+  //   console.log(recipes)
+  let paramsRecipe = await recipes.find((e) => Number(e.id) === Number(id));
+  console.log(paramsRecipe);
+  if (paramsRecipe) {
+    res.status(200).send(paramsRecipe);
+  } else {
+    res.status(404).send("Esta receta no existe");
+  }
+});
+
+router.get("/types", async (req, res) => {
+  let types = await getAllDiets();
+  res.status(200).send(types);
+});
+
+router.post("/recipe", async (req, res) => {
+  const { name, dishRes, dishScore, healthyScore, stepByStep, img, diets } =
+    req.body;
+  
+  let createdRecipe = await Recipe.create({
+    name,
+    dishRes,
+    dishScore,
+    healthyScore,
+    stepByStep,
+    img,
+  });
+  let recipeDiet = await Diet.findAll({
+    where: { name: diets },
+  });
+  createdRecipe.addDiet(recipeDiet);
+  console.log(createdRecipe);
+  res.status(200).send("recipe added successfully");
+  
+});
+
+// GET /recipes
+// GET /recipes/{idReceta}
+// GET /types
+// POST /recipe
 
 module.exports = router;
